72e14e73f3f0e9844e14325cfd34b6395dc15c02
davidyht <160116311+davidyht@users.noreply.github.com>
2025-10-29 17:46:46 +0000

    Deploying to gh-pages from @ davidyht/davidyht.github.io@0c5d44d5b45afa593eae59e1061e1244cf87be63 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,12 +16,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/research/";
           },
-        },{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather.html";
-            },},{id: "news-joining-ucla-sparkles",
+        },{id: "news-joining-ucla-sparkles",
           title: 'Joining UCLA :sparkles:',
           description: "",
           section: "News",},{
