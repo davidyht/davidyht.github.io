a93d79b0153bc909c23121919f88f98b97b25d65
davidyht <160116311+davidyht@users.noreply.github.com>
2026-05-14 22:10:06 +0000

    Deploying to gh-pages from @ davidyht/davidyht.github.io@bf3533e8ae66077a75db0e398bbdf47ea2305581 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4,13 +4,13 @@ const ninja = document.querySelector('ninja-keys');
 // add the home and posts menu items
 ninja.data = [{
     id: "nav-about",
-    title: "about",
+    title: "About",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
     },
   },{id: "nav-research",
-          title: "research",
+          title: "Research",
           description: "Manuscripts in reversed chronological order.",
           section: "Navigation",
           handler: () => {
